63498c520f81287177fc9cce881258607ff64f55
tangratannakra <stela.tasheva@gmail.com>
2019-05-16 10:38:51 +0300

    First

---
+++ Change-color/script.js
@@ -0,0 +1,30 @@
+// create a function that changes randomly the BODY color on event
+//clicking on button;
+const mainBtn = document.querySelector('.mainBtn');
+const bodyElement = document.querySelector('body');
+const info = document.querySelector('p.info');
+
+
+//generate a a value between 0 255
+function generateColor(){
+   return Math.floor(Math.random() * 256) ;
+}
+
+function changeColor() {
+    //on click change color or bodyElement
+    //creates a string with rgb and rgb values for R,G,B
+    //const rgb = "rgb(" + generateColor() + "," + generateColor() + "," + generateColor() + ")";
+    //bodyElement.style.backgroundColor = rgb;
+    let hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let hexColor = '#';
+
+    for (let i=0; i < 6; i++){
+        let random = Math.floor(Math.random() * hexNumbers.length );
+        hexColor += hexNumbers[random];
+    }
+
+    bodyElement.style.backgroundColor = hexColor;
+}
+
+
+mainBtn.addEventListener('click', changeColor);
