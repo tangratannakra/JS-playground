6f7e1f4c603c1c8d468018b947107f999b34bfbb
tangratannakra <stela.tasheva@gmail.com>
2019-05-21 16:48:49 +0300

    Messenger now display data and push data into array when button or enter is pressed. Timestamp also added

--- Messenger/script.js
+++ Messenger/script.js
@@ -2,10 +2,33 @@ const inputBtn = document.querySelector('.sendBtn');
 const inputField = document.querySelector('.input');
 const display = document.querySelector('.history');
 
-let chatHistory = [{
-    timestamp: 'date-time',
-    messege: 'dummy text'
-}];
+let chatHistory = [];
+let chatElement = {};
+chatElement.timeStamp = new Date().toLocaleTimeString();
+    
+//when click event, push into array object and display it into the history
+function whenClicked() {
+    const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+
+    chatElement.messege = inputField.value;
+    chatHistory.push(chatElement);
+    li.appendChild(document.createTextNode(chatElement.timeStamp + " - " + chatElement.messege));
+    display.appendChild(li);
+    inputField.value= ''; //clear the input field for the next messege;
+    console.log(deleteBtn);
+
+}
+
+//when "press to send" is pressed messege is added to history and displayed to the screen
+inputBtn.addEventListener('click', whenClicked);
+
+//when Enter is pressed messege is added to history and displayed to the screen;
+document.addEventListener('keypress', function(e){
+    if (e.keyCode === 13) {
+    whenClicked();
+  }
+});
 
 
 
