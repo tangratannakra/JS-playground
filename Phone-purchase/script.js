750cbd6d9ed90d0cabf10c23ca41ef2ab36562fe
tangratannakra <stela.tasheva@gmail.com>
2019-09-24 17:53:46 +0300

    Uploadign new exercise

---
+++ Phone-purchase/script.js
@@ -0,0 +1,28 @@
+const bankLimit = document.querySelector('.bank-limit');
+const telPrice = document.querySelector('.tel-price');
+
+const calculateBtn = document.querySelector('.calculateBtn');
+const totalPriceDisplay = document.querySelector('.total-price');
+const totalAmountDisplay = document.querySelector('.total-amount');
+
+var telPriceValue = 0;
+var bankLimitValue = 0;
+var totalPrice = 0;
+
+
+calculateBtn.addEventListener('click', e => {
+    e.preventDefault();
+    calculatePrice()
+});
+
+function calculatePrice(){
+    telPriceValue = Number(telPrice.value);
+    bankLimitValue = Number(bankLimit.value);
+    
+    while (totalPrice < bankLimitValue){
+        totalPrice = totalPrice + telPriceValue; 
+    }
+    
+    totalPriceDisplay.innerHTML = totalPrice - telPriceValue;
+    totalAmountDisplay.innerHTML = (totalPrice - telPriceValue)  / telPriceValue;
+}
