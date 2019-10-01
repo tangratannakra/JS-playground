433e9702cde9deab4ceefb2359909957e61b9734
tangratannakra <stela.tasheva@gmail.com>
2019-10-01 14:10:44 +0300

    finishing the task

--- CountingLetters/scripts.js
+++ CountingLetters/scripts.js
@@ -2,17 +2,24 @@
 const stringDisplay = document.querySelector('#string');
 const resultDisplay = document.querySelector('#result');
 const submitBtn = document.querySelector('button');
+
 let input = document.querySelector('input');
-console.log(input);
+
 var inputString;
 var result;
 
-
-submitBtn.onclick = (e) => { 
+//Event on the button
+submitBtn.onclick = (e) => {
+    //preventing the reload of the form 
     e.preventDefault();
+
+    //grabbing the input string from the input field of the form
     inputString = input.value;
     
+    //the function counting the ltters
     countLetters();
+
+    //placing the result into the html element
     resultDisplay.innerHTML = result;
 }
 
@@ -20,14 +27,12 @@ function countLetters() {
     //takes as an imput parametter the imputString
     //and should return the counted letters;
     let splitString = inputString.split('');
-    console.log(splitString);
     let sortString = splitString.sort();
     let count = 1;
     let resultArray = []; 
-    
-    
+
+
      for (let i = 0; i < sortString.length; i++){
-        
         if (sortString[i] == sortString[i+1]){
             count = count + 1;
         }
@@ -40,8 +45,6 @@ function countLetters() {
      }
 
      return result;
-     //console.log(resultArray.join(','));
-
 }
 
 
