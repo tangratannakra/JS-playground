43b5b3eb48b06aa72df21e6389b8a390d56cedac
tangratannakra <stela.tasheva@gmail.com>
2020-03-05 12:23:36 +0200

    back to object solution

--- _Algorithms/SF-2N1-Friends.js
+++ _Algorithms/SF-2N1-Friends.js
@@ -1,3 +1,17 @@
+// In a particular social network friends are automatically allocated to users by the system and users cannot add friends of their choice on their own. There are currently N users on the social network, labeled from 2 to N + 1.
+// For every ith user (where i ranges from 2 to N + 1), the system allocated all the users labeled with multiples of i as the user's friends (if possible).
+// One day, all users of the social network come together for a meeting and form groups such that each person in a group is a direct friend or a friend of friend of every other person of that group.
+// Find the total number of groups.
+
+//Inputs: 5 and 10
+//Outpust: 2 and 3
+
+// Explanation:
+// Two groups will be formed: 2, 3, 4, 6 5
+
+// Explanation :
+// Three groups will be formed: 2, 3, 4, 5, 6, 8, 9, 10
+
 function friendsGroups(n) {
     let groupsFriends = {};
     let noFriends = [];
@@ -8,6 +22,7 @@ function friendsGroups(n) {
             if ((i % user === 0 || user % i === 0) && user !== i) {
                 friends.push(i)
                 groupsFriends[user] = friends;
+
             } else if (i % user === 0) {
                 noFriends.push(user)
             }
@@ -21,4 +36,7 @@ function friendsGroups(n) {
     })}`;
 }
 
-console.log(friendsGroups(20))
+
+//Note it outputs the clusters devided it 2 grups - with friends and no friends
+//need to work on the expressions and result output
+console.log(friendsGroups(10))
